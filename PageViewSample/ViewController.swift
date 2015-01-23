0e66cfe167b63fa8158d8635176d5526ba1d4749
Kazuhiro Hayashi <k.hayashi.info@gmail.com>
2015-01-24 00:09:06 +0900

    Initial Commit

---
+++ PageViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageViewSample
+//
+//  Created by Kazuhiro Hayashi on 1/24/15.
+//  Copyright (c) 2015 Kazuhiro Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PageViewSampleTests/PageViewSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  PageViewSampleTests.swift
+//  PageViewSampleTests
+//
+//  Created by Kazuhiro Hayashi on 1/24/15.
+//  Copyright (c) 2015 Kazuhiro Hayashi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PageViewSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
